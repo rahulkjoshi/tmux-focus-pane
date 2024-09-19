a08defc92c6c81fda4ea291a09f70cb0d2cd081b
Rahul Joshi <rahul.joshi2012@gmail.com>
2024-09-19 00:38:16 -0400

    Exiting focus pane moves in direction specified

--- scripts/event-handler.sh
+++ scripts/event-handler.sh
@@ -1,19 +1,31 @@
 #! /usr/bin/env bash
 #! vi: ft=bash
 
-FOCUS_WINDOW_NAME="focus"
+focus_window_name="focus"
 
 if [[ -z $( tmux show -gqv @focus-restore-command ) ]]; then
     exit
 fi
 
-if [[ "$( tmux list-window -f '#{window_active}' -F "#{==:#W,${FOCUS_WINDOW_NAME}}" )" -eq 1 ]]; then
-    if [[ $( tmux list-pane -t ${FOCUS_WINDOW_NAME} -f '#{pane_active}' -F '#{pane_dead}' ) -eq 0 ]]; then 
+if [[ "$( tmux list-window -f '#{window_active}' -F "#{==:#W,${focus_window_name}}" )" -eq 1 ]]; then
+    if [[ $( tmux list-pane -t "${focus_window_name}" -f '#{pane_active}' -F '#{pane_dead}' ) -eq 0 ]]; then
         # Active window and active pane are the focus pane
         exit
     fi
 fi
 
+move_flag=''
+
+if [[ $( tmux list-pane -t "${focus_window_name}" -f '#{pane_active}' -F '#{pane_at_left}' ) -eq 1 ]]; then
+    move_flag='-L'
+elif [[ $( tmux list-pane -t "${focus_window_name}" -f '#{pane_active}' -F '#{pane_at_right}' ) -eq 1 ]]; then
+    move_flag='-R'
+elif [[ $( tmux list-pane -t "${focus_window_name}" -f '#{pane_active}' -F '#{pane_at_top}' ) -eq 1 ]]; then
+    move_flag='-U'
+elif [[ $( tmux list-pane -t "${focus_window_name}" -f '#{pane_active}' -F '#{pane_at_bottom}' ) -eq 1 ]]; then
+    move_flag='-D'
+fi
+
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-${CURRENT_DIR}/main.sh toggle
+"${CURRENT_DIR}/main.sh" toggle "${move_flag}"
--- scripts/main.sh
+++ scripts/main.sh
@@ -4,20 +4,35 @@
 INSTALL_RESET_HOOK=true
 FOCUS_WINDOW_NAME="focus"
 
+INACTIVE_PANE_BORDER_FMT="fg=color0"
+ACTIVE_PANE_BORDER_FMT="fg=color250"
+
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+function read_vars() {
+    local v;
+    v=$( tmux show -gqv @focus-install-reset-hook )
+    if [[ -n "${v}" && "${v}" =~ (true|false) ]]; then
+        INSTALL_RESET_HOOK="${v}"
+    fi
+}
+
 function toggle_focus() {
-    local restore_command="$( tmux show -gqv @focus-restore-command )"
+    local direction="${1}"
+    local restore_command
+    restore_command="$( tmux show -gqv @focus-restore-command )"
     if [[ -z "${restore_command}" ]]; then
         open_focus
     else
-        reset_focus
+        reset_focus "${direction}"
     fi
 }
 
 function toggle_pane_tag {
-    local focus_pane="$( tmux list-panes -F '#D' -f '#{pane_active}' )"
-    local pane_list="$( tmux show -gqv @focus-tagged-panes )"
+    local focus_pane
+    focus_pane="$( tmux list-panes -F '#D' -f '#{pane_active}' )"
+    local pane_list
+    pane_list="$( tmux show -gqv @focus-tagged-panes )"
 
     if [[ ! "${pane_list}" =~ $focus_pane(,|$) ]]; then
         if [[ -n "${pane_list}" ]]; then
@@ -26,18 +41,21 @@ function toggle_pane_tag {
             pane_list="${focus_pane}"
         fi
     else
-        pane_list="$( echo ${pane_list} | sed -E "s/^${focus_pane},?|,?${focus_pane}//" )"
+        pane_list="$( echo "${pane_list}" | sed -E "s/^${focus_pane},?|,?${focus_pane}//" )"
     fi
     tmux set -g '@focus-tagged-panes' "${pane_list}"
 }
 
 function open_focus() {
-    local focus_pane="$( tmux list-panes -F '#D' -f '#{pane_active}' )"
+    local focus_pane
+    focus_pane="$( tmux list-panes -F '#D' -f '#{pane_active}' )"
+    tmux set -g @focus-pane "${focus_pane}"
     # Create new window named focus
     tmux new-window -n ${FOCUS_WINDOW_NAME} >> /tmp/focus-pane-debug 2>&1
     draw_focus_window
 
-    local temp_pane="$( tmux list-panes -t ${FOCUS_WINDOW_NAME} -F '#{pane_id}' -f '#{pane_active}' )"
+    local temp_pane
+    temp_pane="$( tmux list-panes -t ${FOCUS_WINDOW_NAME} -F '#{pane_id}' -f '#{pane_active}' )"
     local restore_command="tmux swapp -s '${focus_pane}' -t '${temp_pane}'; tmux killw -t ${FOCUS_WINDOW_NAME}"
     tmux set -g @focus-restore-command "${restore_command}" >> /tmp/focus-pane-debug 2>&1
     tmux swapp -s "${focus_pane}" -t "${temp_pane}" >> /tmp/focus-pane-debug 2>&1
@@ -45,22 +63,20 @@ function open_focus() {
 
     # Event hooks to invoke handler when window or pane is changed
     if [[ "${INSTALL_RESET_HOOK}" == "true" ]]; then
-        tmux set-option -og 'window-pane-changed[13]' "run-shell '/usr/bin/env bash ${CURRENT_DIR}/event-handler.sh'"
-        tmux set-option -og 'session-window-changed[13]' "run-shell '/usr/bin/env bash ${CURRENT_DIR}/event-handler.sh'"
+        tmux set-option -og 'window-pane-changed[13]' "run-shell '/usr/bin/env bash ${CURRENT_DIR}/event-handler.sh'" >> /tmp/focus-pane-debug 2>&1
+        tmux set-option -og 'session-window-changed[13]' "run-shell '/usr/bin/env bash ${CURRENT_DIR}/event-handler.sh'" >> /tmp/focus-pane-debug 2>&1
     fi
 }
-
-INACTIVE_PANE_BORDER_FMT="fg=color0"
-ACTIVE_PANE_BORDER_FMT="fg=color250"
 
 function draw_focus_window() {
     if [[ "$( tmux list-panes -t ${FOCUS_WINDOW_NAME} | wc -l )" -ne 1 ]]; then
        tmux kill-pane -a -t "$( tmux list-panes -t ${FOCUS_WINDOW_NAME} -F '#{pane_id}' -f '#{pane_active}' )"
     fi
-    local temp_pane="$( tmux list-panes -t ${FOCUS_WINDOW_NAME} -F '#{pane_id}' )"
+    local temp_pane
+    temp_pane="$( tmux list-panes -t ${FOCUS_WINDOW_NAME} -F '#{pane_id}' )"
     local h_gutter
     local v_gutter
-    IFS=',' read h_gutter v_gutter <<< "$( $CURRENT_DIR/calc.pl )"
+    IFS=',' read -r h_gutter v_gutter <<< "$( "${CURRENT_DIR}/calc.pl" )"
 
     # Partition space around focus
     # Left and right
@@ -80,13 +96,21 @@ function draw_focus_window() {
 }
 
 function reset_focus() {
-    local restore_command="$( tmux show -gqv @focus-restore-command )"
+    local direction="${1}"
+    local restore_command
+    restore_command="$( tmux show -gqv @focus-restore-command )"
     if [[ -z "${restore_command}" ]]; then
         tmux display-message "found no saved restore command"
     fi
+    local focus_pane
+    focus_pane=$( tmux show -gqv @focus-pane )
     remove_hooks
     tmux set -ug @focus-restore-command
+    tmux set -ug @focus-pane
     eval "${restore_command}"
+    if [[ -n "${direction}" ]]; then
+        tmux select-pane -t "${focus_pane}" "${direction}"
+    fi
 }
 
 function remove_hooks() {
@@ -94,17 +118,26 @@ function remove_hooks() {
     tmux set-option -ug 'session-window-changed[13]'
 }
 
+read_vars
+
+cmd=''
+arg1=''
 while [[ -n "$*" ]]; do 
     case $1 in
         toggle )
-            toggle_focus
+            cmd='toggle_focus'
             ;;
         remove-hooks )
-            remove_hooks
+            cmd='remove_hooks'
             ;;
         pane-tag )
-            toggle_pane_tag
+            cmd='toggle_pane_tag'
+            ;;
+        * )
+            arg1="${1}"
             ;;
     esac
     shift
 done
+
+"${cmd}" "${arg1}"
