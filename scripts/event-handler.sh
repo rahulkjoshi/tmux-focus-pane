c182672ca0126bb0b20ca81ad56315eeedab5687
Rahul Joshi <rahul.joshi2012@gmail.com>
2024-10-12 16:45:44 -0400

    Only add toggle dir if pane was changed

--- scripts/event-handler.sh
+++ scripts/event-handler.sh
@@ -8,23 +8,25 @@ function defocus() {
     local focus_pane
     focus_pane=$(tmux show -gqv @tmux-focus-pane)
 
+    local move_flag=''
+
     if [[ "$( tmux list-window -f '#{window_active}' -F "#{==:#W,${focus_window_name}}" )" -eq 1 ]]; then
         if [[ $( tmux list-pane -t "${focus_window_name}" -f '#{pane_active}' -F "#{==:#D,${focus_pane}}" ) -eq 1 ]]; then
             # Active window and active pane are the focus pane
             exit
         fi
-    fi
 
-    local move_flag=''
-
-    if [[ $( tmux list-pane -t "${focus_window_name}" -f '#{pane_active}' -F '#{pane_at_left}' ) -eq 1 ]]; then
-        move_flag='-L'
-    elif [[ $( tmux list-pane -t "${focus_window_name}" -f '#{pane_active}' -F '#{pane_at_right}' ) -eq 1 ]]; then
-        move_flag='-R'
-    elif [[ $( tmux list-pane -t "${focus_window_name}" -f '#{pane_active}' -F '#{pane_at_top}' ) -eq 1 ]]; then
-        move_flag='-U'
-    elif [[ $( tmux list-pane -t "${focus_window_name}" -f '#{pane_active}' -F '#{pane_at_bottom}' ) -eq 1 ]]; then
-        move_flag='-D'
+        # Order matters. Check left and right first because they span the full
+        # height.
+        if [[ $( tmux list-pane -t "${focus_window_name}" -f '#{pane_active}' -F '#{pane_at_left}' ) -eq 1 ]]; then
+            move_flag='-L'
+        elif [[ $( tmux list-pane -t "${focus_window_name}" -f '#{pane_active}' -F '#{pane_at_right}' ) -eq 1 ]]; then
+            move_flag='-R'
+        elif [[ $( tmux list-pane -t "${focus_window_name}" -f '#{pane_active}' -F '#{pane_at_top}' ) -eq 1 ]]; then
+            move_flag='-U'
+        elif [[ $( tmux list-pane -t "${focus_window_name}" -f '#{pane_active}' -F '#{pane_at_bottom}' ) -eq 1 ]]; then
+            move_flag='-D'
+        fi
     fi
 
     echo "callling toggle '${move_flag}'" >> /tmp/tmux-focus-pane-debug
@@ -34,10 +36,10 @@ function defocus() {
 function refocus() {
     local pane_list
     pane_list="$( tmux show -gqv @tmux-focus-tagged-panes )"
-    local focus_pane
-    focus_pane="$( tmux list-panes -F '#D' -f '#{pane_active}' )"
+    local curr_focus
+    curr_focus="$( tmux list-panes -F '#D' -f '#{pane_active}' )"
 
-    if [[ "${pane_list}" =~ $focus_pane(,|$) ]]; then
+    if [[ "${pane_list}" =~ $curr_focus(,|$) ]]; then
         echo "callling toggle" >> /tmp/tmux-focus-pane-debug
         "${CURRENT_DIR}/main.sh" toggle
     fi
